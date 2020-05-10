5de067e8aa756d1e1a32f45257aaf248c060b2af
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-05-10 01:08:40 -0500

    Commit raro

--- main.c
+++ main.c
@@ -7,16 +7,20 @@
 void main()
 {
    int16 iAdc;
+   int16 angle;
+   int16 voltage;
    lcd_init();
    setup_adc_ports(sAN0); //setup_adc_ports(ALL_ANALOG);
    setup_adc(ADC_CLOCK_INTERNAL);
    while (true)
    {
       iAdc = read_adc();
+      voltage = (iAdc * 5) / 1023;
+      angle = (voltage * 270) / 5;
       lcd_gotoxy(1, 1);
-      printf(lcd_putc, "\fVoltaje: %Lu", iAdc*5/1023);
+      printf(lcd_putc, "\fVoltaje: %Lu", voltage);
       lcd_gotoxy(1, 2);
-      printf(lcd_putc, "Angulo: %Lu", iAdc/1023*270);
+      printf(lcd_putc, "Angulo: %Lu", angle);
       delay_ms(100);
    }
 }
