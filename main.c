fd8e869a8c3547b1d1704b796f6117dc8446ae1b
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-05-09 23:37:45 -0500

    Revert "Cambia sAN3 a sAN0"
    This reverts commit 3bdc075cba0f13ca5714c945f46fbf395db46f76.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void main()
 {
    int16 iAdc;
    lcd_init();
-   setup_adc_ports(sAN0);  //setup_adc_ports(ALL_ANALOG);
+   setup_adc_ports(sAN3);  //setup_adc_ports(ALL_ANALOG);
    setup_adc(ADC_CLOCK_INTERNAL);
    while(true)
    {
