6e86372395ca73cda242bfd5615ae81e2d602ecb
Victor Manuel Sanchez Sanchez <vicool275@gmail.com>
2020-05-10 01:07:51 -0500

    Prueba final

--- main.c
+++ main.c
@@ -6,17 +6,21 @@
 
 void main()
 {
-   int16 iAdc;
+   unsigned int16 iAdc;
+   unsigned int16 voltage;
+   unsigned int16 angle;
    lcd_init();
    setup_adc_ports(sAN0); //setup_adc_ports(ALL_ANALOG);
    setup_adc(ADC_CLOCK_INTERNAL);
    while (true)
    {
       iAdc = read_adc();
+      voltage = (iAdc / 1022) * 5;
+      angle = (iAdc / 1022) * 270;
       lcd_gotoxy(1, 1);
-      printf(lcd_putc, "\fVoltaje: %Lu", iAdc*5/1023);
+      printf(lcd_putc, "\fVoltaje: %Lu", voltage);
       lcd_gotoxy(1, 2);
-      printf(lcd_putc, "Angulo: %Lu", iAdc/1023*270);
+      printf(lcd_putc, "Angulo: %Lu", angle);
       delay_ms(100);
    }
 }
