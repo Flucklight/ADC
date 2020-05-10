562d2e0e8c3fc1928ed6fbd1954af8bd24c683af
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-05-09 23:22:42 -0500

    Código de Efren

--- main.c
+++ main.c
@@ -1,29 +1,24 @@
-#include <16f887.h>
-#device ADC = 10
-#fuses INTRC_IO, NOWDT, PROTECT, NOLVP, MCLR, NOBROWNOUT
-#use delay(INTERNAL = 4000000)
-#include <lcd.c>
+#include<16f887.h>
+#device ADC=10
+#fuses INTRC_IO,NOWDT,PROTECT,NOLVP,MCLR,NOBROWNOUT
+#use delay(INTERNAL=4000000)
+
+#include<lcd.c>
 
 void main()
 {
    int16 iAdc;
-   int16 angle;
-   int16 voltage;
-
    lcd_init();
-   setup_adc_ports(sAN3); //setup_adc_ports(ALL_ANALOG);
+   setup_adc_ports(sAN3);  //setup_adc_ports(ALL_ANALOG);
    setup_adc(ADC_CLOCK_INTERNAL);
-
-   while (true)
+   while(true)
    {
-      iAdc = read_adc();
-      angle = (iAdc * 5) / 1023;
-      voltage = (iAdc * 270) / 1023;
-      printf(lcd_putc, "\f");
-      lcd_gotoxy(1, 1);
-      printf(lcd_putc, "Voltaje: %Lu", voltage);
-      lcd_gotoxy(1, 2);
-      printf(lcd_putc, "Angulo: %Lu", angle);
+      iAdc=read_adc();
+      lcd_gotoxy(1,1);
+      printf(lcd_putc,"\fLectura ADC");
+      lcd_gotoxy(1,2);
+      printf(lcd_putc,"Valor: %Lu",iAdc);
       delay_ms(100);
    }
+   
 }
