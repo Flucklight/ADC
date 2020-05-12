2b6fa5f80a465c80f4ac532ee875122bc5bb33be
Julio César Bolio <42160567+jc-bolio@users.noreply.github.com>
2020-05-11 19:21:19 -0500

    Comentarios añadidos

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
-#include <16f887.h> //Incluimos la libreria para el PIC16F887
-#device ADC = 10
+#include <16f887.h> //Incluye la biblioteca para el PIC16F887
+#device ADC = 10 //Configura el convertidor analógico digital con resolución de 10 bits
 #fuses INTRC_IO, NOWDT, PROTECT, NOLVP, MCLR, NOBROWNOUT //Configuramos los registros del PIC
-#use delay(INTERNAL = 4000000) //Configuramos el reloj interno a 4MHz
-#include <lcd.c> //Incluimos la libreria de la pantalla LCD
+#use delay(INTERNAL = 4000000) //Configura el reloj interno a 4MHz
+#include <lcd.c> //Incluye la libreria de la pantalla LCD
 
 void main(){
-   int32 iAdc, voltage, angle; //Declaramos variables
+   int32 iAdc, voltage, angle; //Declaramos variables de 32 bits
    lcd_init(); //Inicialización de la pantalla LCD
-   setup_adc_ports(sAN0); //setup_adc_ports(ALL_ANALOG);
-   setup_adc(ADC_CLOCK_INTERNAL); 
+   setup_adc_ports(sAN0); //Configura la entrada analógica 0 del microcontrolador
+   setup_adc(ADC_CLOCK_INTERNAL); //Configura frecuencia de muestro con reloj interno
    
    while (true){
-      iAdc = read_adc();
+      iAdc = read_adc(); //Obtiene valor del ADC en ese momento y lo guarda en iADC
       voltage = (iAdc * 5) / 1023; //Convierte el valor del ADC a voltaje
       lcd_gotoxy(1, 1); //Mueve el apuntador interno a la 1ra fila
       printf(lcd_putc, "\fVoltaje: %Lu", voltage); //Imprime el valor del voltaje
